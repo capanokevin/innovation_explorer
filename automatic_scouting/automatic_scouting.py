81e0fe762c107fd5cce71dc1dd306b68b171883f
Kevin Capano <81310090+capanokevin@users.noreply.github.com>
2024-08-27 14:23:53 +0200

    Update automatic_scouting.py

--- automatic_scouting/automatic_scouting.py
+++ automatic_scouting/automatic_scouting.py
@@ -834,6 +834,13 @@ def search_pitchbook(startup_name, similarity_threshold=0.6):
 ##### POSTGRES DATABASE #####
 ##############################
 # Function to establish a connection to the PostgreSQL database
+
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
 def connect_db():
     try:
         conn = psycopg2.connect(
